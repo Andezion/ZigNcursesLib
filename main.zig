05300bed11a3e33cf363f4db05f5486a7fe9a0aa
Andezion <245122@edu.p.lodz.pl>
2026-05-28 13:02:22 +0200

    add: added basic info

---
+++ main.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+const term = @import("src/term.zig");
+
+pub fn main() !void {
+    try term.clear();
+    try term.hideCursor();
+    defer term.showCursor();
+
+    try term.moveCursor(10, 5);
+    try term.setFgColor256(33);
+    try term.setBgColor256(226);
+    try term.print("Hello, Zig ncurses-like! Press any key to exit.\n", .{});
+    try term.reset();
+
+    var stdin = std.io.getStdIn().reader();
+    _ = try stdin.readByte();
+}
