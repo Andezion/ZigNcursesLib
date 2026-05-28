1a2bcb92eab57ea19e9c62cfd5626d614faf93eb
Linus <torvalds@linux-foundation.org>
2026-05-28 13:12:40 +0200

    fix: fixed version typo

--- src/term.zig
+++ src/term.zig
@@ -5,36 +5,29 @@ pub fn clear() !void {
 }
 
 pub fn moveCursor(x: usize, y: usize) !void {
-    const out = std.io.getStdOut().writer();
-    try out.print("\x1b[{d};{d}H", .{ y, x });
+    try std.fs.File.stdout().writeAll("\x1b[{d};{d}H", .{ y, x });
 }
 
 pub fn setFgColor256(n: u8) !void {
-    const out = std.io.getStdOut().writer();
-    try out.print("\x1b[38;5;{d}m", .{n});
+    try std.fs.File.stdout().writeAll("\x1b[38;5;{d}m", .{n});
 }
 
 pub fn setBgColor256(n: u8) !void {
-    const out = std.io.getStdOut().writer();
-    try out.print("\x1b[48;5;{d}m", .{n});
+    try std.fs.File.stdout().writeAll("\x1b[48;5;{d}m", .{n});
 }
 
 pub fn reset() !void {
-    const out = std.io.getStdOut().writer();
-    try out.print("\x1b[0m", .{});
+    try std.fs.File.stdout().writeAll("\x1b[0m", .{});
 }
 
 pub fn hideCursor() !void {
-    const out = std.io.getStdOut().writer();
-    try out.print("\x1b[?25l", .{});
+    try std.fs.File.stdout().writeAll("\x1b[?25l", .{});
 }
 
 pub fn showCursor() !void {
-    const out = std.io.getStdOut().writer();
-    try out.print("\x1b[?25h", .{});
+    try std.fs.File.stdout().writeAll("\x1b[?25h", .{});
 }
 
 pub fn print(comptime fmt: []const u8, args: anytype) !void {
-    const out = std.io.getStdOut().writer();
-    try out.print(fmt, args);
+    try std.fs.File.stdout().writeAll(fmt, args);
 }
