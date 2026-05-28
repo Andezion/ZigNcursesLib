31d7b3984927c180a8e364ec3cba8f8958fa989e
Linus <torvalds@linux-foundation.org>
2026-05-28 13:18:21 +0200

    fix: corrected version issues

--- src/term.zig
+++ src/term.zig
@@ -1,33 +1,45 @@
 const std = @import("std");
 
 pub fn clear() !void {
-    try std.fs.File.stdout().writeAll("\x1b[2J\x1b[H", .{});
+    try std.fs.File.stdout().writeAll("\x1b[2J\x1b[H");
 }
 
 pub fn moveCursor(x: usize, y: usize) !void {
-    try std.fs.File.stdout().writeAll("\x1b[{d};{d}H", .{ y, x });
+    const allocator = std.heap.page_allocator;
+    const s = try std.fmt.allocPrint(allocator, "\x1b[{d};{d}H", .{ y, x });
+    defer allocator.free(s);
+    try std.fs.File.stdout().writeAll(s);
 }
 
 pub fn setFgColor256(n: u8) !void {
-    try std.fs.File.stdout().writeAll("\x1b[38;5;{d}m", .{n});
+    const allocator = std.heap.page_allocator;
+    const s = try std.fmt.allocPrint(allocator, "\x1b[38;5;{d}m", .{n});
+    defer allocator.free(s);
+    try std.fs.File.stdout().writeAll(s);
 }
 
 pub fn setBgColor256(n: u8) !void {
-    try std.fs.File.stdout().writeAll("\x1b[48;5;{d}m", .{n});
+    const allocator = std.heap.page_allocator;
+    const s = try std.fmt.allocPrint(allocator, "\x1b[48;5;{d}m", .{n});
+    defer allocator.free(s);
+    try std.fs.File.stdout().writeAll(s);
 }
 
 pub fn reset() !void {
-    try std.fs.File.stdout().writeAll("\x1b[0m", .{});
+    try std.fs.File.stdout().writeAll("\x1b[0m");
 }
 
 pub fn hideCursor() !void {
-    try std.fs.File.stdout().writeAll("\x1b[?25l", .{});
+    try std.fs.File.stdout().writeAll("\x1b[?25l");
 }
 
 pub fn showCursor() !void {
-    try std.fs.File.stdout().writeAll("\x1b[?25h", .{});
+    try std.fs.File.stdout().writeAll("\x1b[?25h");
 }
 
 pub fn print(comptime fmt: []const u8, args: anytype) !void {
-    try std.fs.File.stdout().writeAll(fmt, args);
+    const allocator = std.heap.page_allocator;
+    const s = try std.fmt.allocPrint(allocator, fmt, args);
+    defer allocator.free(s);
+    try std.fs.File.stdout().writeAll(s);
 }
